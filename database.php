a184eb92dec55a289974624dac9259e9cc8c547c
RwanS <rwan.salah91@gmail.com>
2016-11-23 11:51:39 -0500

    Update database.php

--- database.php
+++ database.php
@@ -28,19 +28,22 @@ phone_number NUMBER NOT NULL,
 address VARCHAR2(50) NOT NULL,
 city VARCHAR2(50) NOT NULL,
 zip VARCHAR2(10) NOT NULL,
-child_Fname VARCHAR2(50) NOT NULL,
-child_Lname VARCHAR2(50) NOT NULL,
+child_fname VARCHAR2(50) NOT NULL,
+child_lname VARCHAR2(50) NOT NULL,
 )";
 
 $sql = "CREATE TABLE MyChild (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-cfirstname VARCHAR2(30) NOT NULL,
-clastname VARCHAR2(30) NOT NULL,
-allergies VARCHAR2(50),
+child_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+child_fname VARCHAR2(30) NOT NULL,
+child_lname VARCHAR2(30) NOT NULL,
 phone_number NUMBER NOT NULL,
 address VARCHAR2(50) NOT NULL,
-parent_Fname VARCHAR2(50) NOT NULL,
-parent_Lname VARCHAR2(50) NOT NULL,
+city VARCHAR2(50) NOT NULL,
+zip VARCHAR2(10) NOT NULL,
+parent_fname VARCHAR2(50) NOT NULL,
+parent_lname VARCHAR2(50) NOT NULL,
+info VARCHAR2(200),
+allergies VARCHAR2(50),
 child_dob DATE NOT NULL
 )";
 
@@ -56,53 +59,53 @@ if ($conn->query($sql) === TRUE) {
 }
 
 
-//Insert Mychild Table
-$id = mysqli_real_escape_string($link, $_POST['id']);
-$cfirstname = mysqli_real_escape_string($link, $_POST['firstName']);
-$clastname = mysqli_real_escape_string($link, $_POST['']);
-$allergies = mysqli_real_escape_string($link, $_POST['']);
-$phone_number = mysqli_real_escape_string($link, $_POST['']);
-$address = mysqli_real_escape_string($link, $_POST['']);
-$parent_Fname = mysqli_real_escape_string($link, $_POST['']);
-$parent_Lname = mysqli_real_escape_string($link, $_POST['']);
-$child_dob = mysqli_real_escape_string($link, $_POST['']);
-$info = mysqli_real_escape_string($link, $_POST['']);
+//Insert into Table MyChild 
+$id = mysqli_real_escape_string($conn, $_POST['id from html form']);
+$firstname = mysqli_real_escape_string($conn, $_POST['firstName']);
+$lastname = mysqli_real_escape_string($conn, $_POST['']);
+$phone_number = mysqli_real_escape_string($conn, $_POST['']);
+$address = mysqli_real_escape_string($conn, $_POST['']);
+$city = mysqli_real_escape_string($conn, $_POST['']);
+$zip = mysqli_real_escape_string($conn, $_POST['']);
+$parent_fname = mysqli_real_escape_string($conn, $_POST['']);
+$parent_lname = mysqli_real_escape_string($conn, $_POST['']);
+$info = mysqli_real_escape_string($conn, $_POST['']);
+$allergies = mysqli_real_escape_string($conn, $_POST['']);
+$child_dob = mysqli_real_escape_string($conn, $_POST['']);
 
-$sql = "INSERT INTO MyChild (id, cfirstname, clastname, allergies, phone_number, address, parent_Fname, parent_Lname, child_dob, info) 
-					VALUES ('$id', '$cfirstname', '$clastname', '$allergies', '$phone_number', '$address', '$parent_Fname', '$parent_Lname', '$child_dob', '$info')";
+
+$sql = "INSERT INTO MyChild (child_id, child_fname, child_lname, phone_number, address, city, zip, parent_fname, parent_lname, info, allergies, child_dob) 
+					VALUES ('$id', '$firstname', '$lastname', '$phone_number', '$address', '$city', '$zip','$parent_fname', '$parent_lname', '$info', '$allergies', '$child_dob')";
 
 
 //Insert MyParent Table
-$parent_id = mysqli_real_escape_string($link, $_POST['']);
-$firstname = mysqli_real_escape_string($link, $_POST['']);
-$lastname = mysqli_real_escape_string($link, $_POST['']);
-$email = mysqli_real_escape_string($link, $_POST['']);
-$phone_number = mysqli_real_escape_string($link, $_POST['']);
-$address = mysqli_real_escape_string($link, $_POST['']);
-$city = mysqli_real_escape_string($link, $_POST['']);
-$zip = mysqli_real_escape_string($link, $_POST['']);
-$child_Fname = mysqli_real_escape_string($link, $_POST['']);
-$child_Lname = mysqli_real_escape_string($link, $_POST['']);
+$id = mysqli_real_escape_string($conn, $_POST['']);
+$firstname = mysqli_real_escape_string($conn, $_POST['']);
+$lastname = mysqli_real_escape_string($conn, $_POST['']);
+$email = mysqli_real_escape_string($conn, $_POST['']);
+$phone_number = mysqli_real_escape_string($conn, $_POST['']);
+$address = mysqli_real_escape_string($conn, $_POST['']);
+$city = mysqli_real_escape_string($conn, $_POST['']);
+$zip = mysqli_real_escape_string($conn, $_POST['']);
+$child_fname = mysqli_real_escape_string($conn, $_POST['']);
+$child_lname = mysqli_real_escape_string($conn, $_POST['']);
 
 
-$sql = "INSERT INTO MyParent (parent_id, firstname, lastname, email, phone_number, address, city, zip, child_Fname, child_Lname) 
-					VALUES ('$parent_id', '$cfirstname', '$clastname', '$email', '$phone_number', '$address', '$city', '$zip', '$child_Fname', '$child_Lname')";
+$sql = "INSERT INTO MyParent (parent_id, firstname, lastname, email, phone_number, address, city, zip, child_fname, child_lname) 
+					VALUES ('$id', '$firstname', '$lastname', '$email', '$phone_number', '$address', '$city', '$zip', '$child_fname', '$child_lname')";
 
 	
 //Delete MyChild
-$sql = "DELETE FROM MyChild WHERE id = $id";
+$sql = "DELETE FROM MyChild WHERE child_id = $id";
 
 //Delete MyParent
-$sql = "DELETE FROM MyParent WHERE parent_id = $parent_id";
+$sql = "DELETE FROM MyParent WHERE parent_id = $id";
 
 //Update MyChild
-$sql = "UPDATE MyChild SET cfirstname = '$cfirstname' WHERE id = '$id'";
+$sql = "UPDATE MyChild SET child_fname = '$firstname' WHERE child_id = '$id'";
 
 //Update MyParent
-$sql = "UPDATE MyParent SET firstname = '$firstname' WHERE parent_id = '$parent_id'";
-
-
-
+$sql = "UPDATE MyParent SET firstname = '$firstname' WHERE parent_id = '$id'";
 
 
 $conn->close();
