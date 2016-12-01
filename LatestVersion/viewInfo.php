c623c9f46e210c6e9d601fd1940edd8fbd938345
RwanS <rwan.salah91@gmail.com>
2016-12-01 15:52:51 -0500

    Update viewInfo.php

--- LatestVersion/viewInfo.php
+++ LatestVersion/viewInfo.php
@@ -78,7 +78,7 @@
 		  <div id="right">
 			  <div class="form-group">
 				<label>Allergies</label>
-				<textarea class="form-control" name = "allergies" rows="4" placeholder = "Write text" value = "<?php echo $_GET['allergies']?>"></textarea>
+				<textarea class="form-control" name = "allergies" rows="4" placeholder = "<?php echo $_GET['allergies']?>"></textarea>
 			  </div>
 			  <div class="form-group">
 				<label>Phone Number</label>
