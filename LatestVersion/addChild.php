64e6c013600cd042fa879f5eb4beb0ef0525c5e4
conlizard <mcw396@hotmail.com>
2016-12-01 22:37:13 -0500

    Add files via upload
    addChild with validation

--- LatestVersion/addChild.php
+++ LatestVersion/addChild.php
@@ -1,3 +1,145 @@
+<?php
+// define variables and set to empty values
+$zipErr = $cfirstnameErr = $childLastNameErr = $parentfnameErr = $parentlnameErr = $bdayErr = $phoneNumErr = $addressErr = $cityErr = "";
+$zip = $cfirstname = $childLastName = $parentfname = $parentlname = $bday = $phoneNum = $address = $city = $allergies = "";
+$isValid = true; 
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["cfirstname"])) {
+    $cfirstnameErr = "First name is required";
+  } else {
+    $cfirstname = test_input($_POST["cfirstname"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$cfirstname)) {
+      $cfirstnameErr = "Only letters and white space allowed";
+	  $isValid = false; 
+    }
+	if(strlen($cfirstname) < 2)
+	{
+		$cfirstnameErr = "Name must be at least 2 characters long";
+		$isValid = false; 
+	}
+	
+  }
+  
+  if (empty($_POST["clastname"])) {
+    $childLastNameErr = "Last name is required";
+	$isValid = false; 
+  } else {
+    $childLastName = test_input($_POST["clastname"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$childLastName)) {
+      $childLastNameErr = "Only letters and white space allowed"; 
+	  $isValid = false; 
+    }
+	if(strlen($childLastName) < 2)
+	{
+		$childLastNameErr = "Name must be at least 2 characters long";
+		$isValid = false; 
+	}
+  }
+  
+  if (empty($_POST["parent_Fname"])) {
+    $parentfnameErr = "Parent first name is required";
+	$isValid = false; 
+  } else {
+    $parentfname = test_input($_POST["parent_Fname"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$parentfname)) {
+      $parentfnameErr = "Only letters and white space allowed";
+		$isValid = false; 	  
+    }
+	if(strlen($parentfname) < 2)
+	{
+		$parentfnameErr = "Name must be at least 2 characters long";
+		$isValid = false; 
+	}	
+  }
+  
+  if (empty($_POST["parent_Lname"])) {
+    $parentlnameErr = "Parent last name is required";
+	$isValid = false; 
+  } else {
+    $parentlname = test_input($_POST["parent_Lname"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$parentlname)) {
+      $parentlnameErr = "Only letters and white space allowed"; 
+	  $isValid = false; 
+    }
+	if(strlen($parentlname) < 2)
+	{
+		$parentlnameErr = "Name must be at least 2 characters long";
+		$isValid = false; 
+	}
+  }
+  
+  if (empty($_POST["date_of_birth"])) {
+    $bdayErr = "Birthday is required";
+	$isValid = false;
+  }
+  else 
+		$bday = $_POST["date_of_birth"];
+    
+  if (empty($_POST["phone_number"])) {
+    $phoneNumErr = "Phone number is required";
+	$isValid = false; 
+  } else {
+    $phoneNum = test_input($_POST["phone_number"]);
+    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+    if (!preg_match("%\([0-9]{3}\)[- ]?[0-9]{3}-[0-9]{4}%",$phoneNum)) {
+      $phoneNumErr = "Invalid phone number"; 
+	  $isValid = false; 
+    }
+  }
+  
+  if (empty($_POST["city"])) {
+    $cityErr = "City/Town is required";
+	$isValid = false; 
+  } else {
+    $city = test_input($_POST["city"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$city)) {
+      $cityErr = "Only letters and white space allowed"; 
+	  $isValid = false; 
+    }
+  }
+
+  if (empty($_POST["address"])) {
+    $addressErr = "Address is required";
+	$isValid = false; 
+  } else {
+    $address = test_input($_POST["address"]);
+	if (!preg_match("/^[0-9 a-zA-Z .-]*$/",$address)) {
+      $addressErr = "Invalid address"; 
+	  $isValid = false; 
+    }
+  }
+  
+  if (empty($_POST["zip"])) {
+    $zipErr = "Zip is required";
+	$isValid = false; 
+  } else {
+    $zip = test_input($_POST["zip"]);
+  if (!preg_match("%[ABCEGHJKLMNPRSTVXY]{1}\d{1}[A-Z]{1} \d{1}[A-Z]{1}\d{1}%",$zip)) {
+      $zipErr = "Invalid zip"; 
+	  $isValid = false; 
+    }
+  }
+
+  if (!empty($_POST["allergies"])) {
+    $allergies = $_POST["allergies"];
+  }
+}
+
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
 <!DOCTYPE>
 <html>
 <head>
@@ -17,6 +159,9 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>-->
 
 	<title>Adding a child</title>
+	<style>
+		.error {color: #FF0000;}
+	</style>
 </head>
 <body class="container"> 
 	
@@ -56,23 +201,28 @@
 	<div id="left">
 		<div class="form-group">
 			<label>First Name</label>
-			<input type="text" name = "cfirstname" class="form-control" >
+			<input type="text" name = "cfirstname" class="form-control" value="<?php echo $cfirstname;?>">
+			<span class="error"> <?php echo $cfirstnameErr;?></span>
 		  </div>
 		  <div class="form-group">
 			<label>Last name</label>
-			<input type="text" name = "clastname" class="form-control" >
+			<input type="text" name = "clastname" class="form-control" value="<?php echo $childLastName;?>">
+			<span class="error"> <?php echo $childLastNameErr;?></span>
 		  </div>
 		  <div class="form-group">
 			<label>Parent's First Name</label>
-			<input type="text" name = "parent_Fname" class="form-control" >
+			<input type="text" name = "parent_Fname" class="form-control" value="<?php echo $parentfname;?>">
+			<span class="error"> <?php echo $parentfnameErr;?></span>
 		  </div>
 		  <div class="form-group">
 			<label>Parent's Last Name</label>
-			<input type="text" name = "parent_Lname" class="form-control" >
+			<input type="text" name = "parent_Lname" class="form-control" value="<?php echo $parentlname;?>">
+			<span class="error"> <?php echo $parentlnameErr;?></span>
 		  </div>
 		  <div class="form-group">
 			<label>Date of Birth</label>
-			<input type="date" name = "date_of_birth" class="form-control" id="dateField" onkeydown="return false"> <!-- Channge the value of the name tag if need be -->
+			<input type="date" name = "date_of_birth" class="form-control" id="dateField" onkeydown="return false" value="<?php echo $bday;?>"> <!-- Channge the value of the name tag if need be -->
+			<span class="error"> <?php echo $bdayErr;?></span> <!-- Channge the value of the name tag if need be -->
 			<script type="text/javascript">
 			/* Sets the minimum and maximum dates of birth*/
 				var today = new Date();
@@ -101,19 +251,23 @@
 		  </div>
 		  <div class="form-group">
 			<label>Phone Number</label>
-			<input type="text" name = "phone_number" class="form-control" >
+			<input type="text" name = "phone_number" class="form-control" maxlength = "14" placeholder = "e.g.: (555) 555-5555" value="<?php echo $phoneNum;?>">
+			<span class="error"> <?php echo $phoneNumErr;?></span>
 		  </div>
 		  <div class="form-group">
 			<label>Address</label>
-			<input type="text" name = "address" class="form-control" >
+			<input type="text" name = "address" class="form-control" value="<?php echo $address;?>">
+			<span class="error"> <?php echo $addressErr;?></span>
 		  </div>
 		  <div class="form-group">
 			<label>City/Town</label>
-			<input type="text" name = "city" class="form-control" >
+			<input type="text" name = "city" class="form-control" value="<?php echo $city;?>">
+			<span class="error"> <?php echo $cityErr;?></span>
 		  </div>
 		  <div class="form-group">
 			<label>Zip</label>
-			<input type="text" name = "zip" class="form-control" >
+			<input type="text" name = "zip" class="form-control" maxlength = "7" value="<?php echo $zip;?>">
+			<span class="error"> <?php echo $zipErr;?></span>
 	</div>
 	  </div>
 	  
@@ -123,29 +277,32 @@
 	</form>
 	 
 
-	<?php      
-	if (isset($_POST['submit'])){
-		
-		$conn = mysql_connect("localhost", "username", "password");
-		
-		//check connection 
-		if (!$conn)
+	<?php 
+		if($isValid == true)
 		{
-			die("Cannot connect: " . mysql_error());	
-		}
-		
-		mysql_select_db("myDaycare", $conn);
-		
-		$addChild = "INSERT INTO myChild(firstName, lastName, parent_Fname, parent_Lname, date_of_birth, allergies, phone_number, address, city, zip) 
-					 VALUES ('$_POST[cfirstname]', '$_POST[clastname]', '$_POST[parent_Fname]', '$_POST[parent_Lname]',
-					 '$_POST[date_of_birth]','$_POST[allergies]',  '$_POST[phone_number]', '$_POST[address]', '$_POST[city]', '$_POST[zip]')";
-		
+			if (isset($_POST['submit'])){
+				
+				$conn = mysql_connect("localhost", "username", "password");
+				
+				//check connection 
+				if (!$conn)
+				{
+					die("Cannot connect: " . mysql_error());	
+				}
+				
+				mysql_select_db("myDaycare", $conn);
+				
+				$addChild = "INSERT INTO myChild(firstName, lastName, parent_Fname, parent_Lname, date_of_birth, allergies, phone_number, address, city, zip) 
+							 VALUES ('$_POST[cfirstname]', '$_POST[clastname]', '$_POST[parent_Fname]', '$_POST[parent_Lname]',
+							 '$_POST[date_of_birth]','$_POST[allergies]',  '$_POST[phone_number]', '$_POST[address]', '$_POST[city]', '$_POST[zip]')";
+				
+					
+				mysql_query($addChild, $conn);
+				
+				mysql_close($conn);
+			}
 			
-		mysql_query($addChild, $conn);
-		
-		mysql_close($conn);
-		
-	}
+		}
 		
 	?>
 
